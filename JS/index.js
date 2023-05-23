7c907403f9dabc23bf23bd42bdccd55f313a7e32
ellie489 <114486930+ellie489@users.noreply.github.com>
2023-05-23 23:31:02 +0200

    Carousel/Slider
    Made a new slider for latest posts

--- JS/index.js
+++ JS/index.js
@@ -1,7 +1,11 @@
-const slides = document.querySelectorAll(".slide");
-const nextSlide = document.querySelector(".slider-button.next");
-const prevSlide = document.querySelector(".slider-button.prev");
+// const slides = document.querySelectorAll(".slide");
+// const nextSlide = document.querySelector(".slider-button.next");
+// const prevSlide = document.querySelector(".slider-button.prev");
 const url = "https://exam.squareeyes-project.no/wp-json/wp/v2/posts?_embed";
+const slidesContainer = document.getElementById("slides-container");
+const slide = document.querySelector(".slide");
+const prevButton = document.getElementById("slide-arrow-prev");
+const nextButton = document.getElementById("slide-arrow-next");
 
 async function getBlogPosts() {
 
@@ -27,10 +31,11 @@ async function renderPosts() {
   function createPost(posts) {
 
     posts.forEach( post => {
-      const sliderContainer = document.querySelector(".slider-container");
-      const slider = document.createElement("div");
-      slider.className = "slider";
-      const slide = document.createElement("div");
+    //   const sliderContainer = document.querySelector(".slider-container");
+    //   const slider = document.createElement("div");
+    //   slider.className = "slider";
+      const slider = document.querySelector(".slides-container");
+      const slide = document.createElement("li");
       slide.className = "slide";
       const link = document.createElement("a");
       link.href = "blog-post.html?id=" + post.id;
@@ -41,44 +46,59 @@ async function renderPosts() {
       image.src = post._embedded["wp:featuredmedia"][0].source_url;
       image.alt = post._embedded["wp:featuredmedia"][0].alt_text;
       title.innerText = post.title.rendered;
-      link.append(title, image);
+      link.append(image, title);
       slide.append(link);
-      sliderContainer.append(slide);
+      slider.append(slide);
+    //   sliderContainer.append(slide);
       }
     )
   }
 
 
-let currentSlide = 0;
-let maxSlide = slides.length - 1;
+  nextButton.addEventListener("click", (event) => {
 
-slides.forEach((slide, index) => {
-  slide.style.transform = `translateX(${index * 100}%)`;
-});
-
-nextSlide.addEventListener("click", function () {
-  if (currentSlide === maxSlide) {
-    currentSlide = 0;
-  } else {
-    currentSlide++;
-  };
-
-slides.forEach((slide, index) => {
- slide.style.transform = `translateX(${100 * (index - currentSlide)}%)`;
-});
-});
-
-prevSlide.addEventListener("click", function () {
-  if (currentSlide === 0) {
-    currentSlide = maxSlide;
-  } else {
-    currentSlide--;
-  }
-
-  slides.forEach((slide, index) => {
-    slide.style.transform = `translateX(${100 * (index - currentSlide)}%)`;
+    // const slideWidth = slide.clientWidth;
+  
+      slidesContainer.scrollLeft += 500;
+  
   });
-});
+
+  prevButton.addEventListener("click", () => {
+
+    const slideWidth = slide.clientWidth;
+  
+    slidesContainer.scrollLeft -= 500;
+  
+  });
+  
+
+// slides.forEach((slide, index) => {
+//   slide.style.transform = `translateX(${index * 100}%)`;
+// });
+
+// nextSlide.addEventListener("click", function () {
+//   if (currentSlide === maxSlide) {
+//     currentSlide = 0;
+//   } else {
+//     currentSlide++;
+//   };
+
+// slides.forEach((slide, index) => {
+//  slide.style.transform = `translateX(${100 * (index - currentSlide)}%)`;
+// });
+// });
+
+// prevSlide.addEventListener("click", function () {
+//   if (currentSlide === 0) {
+//     currentSlide = maxSlide;
+//   } else {
+//     currentSlide--;
+//   }
+
+//   slides.forEach((slide, index) => {
+//     slide.style.transform = `translateX(${100 * (index - currentSlide)}%)`;
+//   });
+// });
 
 
 renderPosts();
